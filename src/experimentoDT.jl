13655bffc58ff0a7e94ea8de737c71f214bd864b
irenetembras <tembrasdiazirene3@gmail.com>
2025-05-07 13:38:25 +0200

    test ANOVA dt

--- src/experimentoDT.jl
+++ src/experimentoDT.jl
@@ -1,6 +1,6 @@
 module experimentoDT
 
-using CSV, DataFrames, DecisionTree, Statistics
+using CSV, DataFrames, DecisionTree, Statistics, HypothesisTests
 include("P1_soluciones.jl") # Asegúrate de tener crossvalidation y normalizeMinMax disponibles
 
 function normalizacionDecisionTree(inputs::Matrix{<:Real})
@@ -49,6 +49,38 @@ function ejecutarDecisionTree()
     end
 
     CSV.write("resultados_crossval_dt.csv", resultados)
+
+    # Llamar al test ANOVA solo para Accuracy
+    realizar_anova(resultados)
+
+end
+
+# Función para realizar el test ANOVA sobre Accuracy
+function realizar_anova(df::DataFrame, colname::Symbol = :Accuracy)
+    # Expandir los datos por fold (solo para Accuracy)
+    expanded = expand_metric_column(df, colname)
+
+    # Agrupar los valores por profundidad
+    grupos = [expanded[expanded.Profundidad .== profundidad, :Valor] for profundidad in unique(expanded.Profundidad)]
+
+    # Ejecutar el test ANOVA
+    anova_result = OneWayANOVATest(grupos...)
+    
+    println("Resultados del test ANOVA para Accuracy: ")
+    println(anova_result)
+end
+
+# Función para expandir los valores de la columna de métricas
+function expand_metric_column(df::DataFrame, colname::Symbol)
+    expanded_data = DataFrame(Profundidad = Int[], Fold = Int[], Valor = Float64[])
+    
+    for (i, row) in enumerate(eachrow(df))
+        metric_values = row[colname]
+        for (j, val) in enumerate(metric_values)
+            push!(expanded_data, (df.Profundidad[i], j, val))
+        end
+    end
+    return expanded_data
 end
 
 end # module
